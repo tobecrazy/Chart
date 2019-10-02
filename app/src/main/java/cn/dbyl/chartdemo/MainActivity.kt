9bd4d3f8cca9f89a1463911c45f09d54f826eac1
Young <young.liu@sap.com>
2019-10-02 22:47:16 +0800

    add demo code

--- app/src/main/java/cn/dbyl/chartdemo/MainActivity.kt
+++ app/src/main/java/cn/dbyl/chartdemo/MainActivity.kt
@@ -10,8 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        donut_chart_view.progressValue=30
-
-
+        start.setOnClickListener{
+            donut_chart_view.progressValue=70
+            donut_chart_view.isAnimation=true
+            donut_chart_view.interval=3000
+        }
     }
 }
